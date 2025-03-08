0da7260c45a4a3e1857e08fe09e5e11f0febc649
BROMEROPK12 <bromero12@uan.edu.co>
2025-03-08 11:43:38 -0500

    nuevos archivos para Kotlin

---
+++ app/src/main/java/com/bankcol/BankColApp.kt
@@ -0,0 +1,7 @@
+package com.bankcol
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class BankColApp : Application()
---
+++ app/src/main/java/com/bankcol/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.bankcol
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            // Tu UI aquí
+        }
+    }
+}
